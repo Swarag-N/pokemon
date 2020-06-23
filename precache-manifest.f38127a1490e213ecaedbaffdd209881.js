9a1aaf1bc870c97b25fcf177b4c8dce31b72bca2
whois-this <46181010+whois-this@users.noreply.github.com>
2020-06-23 13:05:48 +0530

    Updates

---
+++ precache-manifest.f38127a1490e213ecaedbaffdd209881.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "459d45225b83910d35f06639f12e7d5b",
+    "url": "/pokemon/index.html"
+  },
+  {
+    "revision": "e500003ae53d5a4b2b4c",
+    "url": "/pokemon/static/css/main.df25969d.chunk.css"
+  },
+  {
+    "revision": "c3ceb4d4beb4bfd3ce50",
+    "url": "/pokemon/static/js/2.2a05240f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokemon/static/js/2.2a05240f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e500003ae53d5a4b2b4c",
+    "url": "/pokemon/static/js/main.4ab496ca.chunk.js"
+  },
+  {
+    "revision": "bd40de3849ed1c334612",
+    "url": "/pokemon/static/js/runtime-main.f8aee566.js"
+  }
+]);
